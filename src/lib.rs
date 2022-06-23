210f12c5f6bead4f8aa3fddcefbd70b7ebaa8495
Leejay Hsu <leejayhsu@gmail.com>
2022-06-22 23:53:54 -0700

    date works

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,22 @@
+#![allow(unused)]
+use chrono::{DateTime, NaiveDate, Utc};
 use log::{debug, info};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Default, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 
 pub struct NachaFile {
-    pub file_header: FileHeader,
+    pub file_header: Option<FileHeader>,
     pub batches: Vec<Batch>,
-    pub file_control: FileControl,
+    pub file_control: Option<FileControl>,
 }
 
 impl NachaFile {
     pub fn new(content: String) -> NachaFile {
         let mut file = NachaFile {
-            ..Default::default()
+            file_header: None,
+            batches: Vec::new(),
+            file_control: None,
         };
 
         for linestr in content.lines() {
@@ -21,34 +25,27 @@ impl NachaFile {
             match record_type {
                 "1" => {
                     debug!("file header found");
-                    file.file_header = FileHeader {
-                        ..Default::default()
-                    };
-                    file.file_header.parse(line);
+                    file.file_header = Some(FileHeader::parse(line))
                 }
                 "5" => {
                     debug!("batch header found");
-                    let mut batch: Batch = Default::default();
-                    batch.batch_header = BatchHeader {
-                        ..Default::default()
+                    let batch = Batch {
+                        batch_header: BatchHeader::parse(line),
+                        detail_entries: Vec::new(),
                     };
-                    batch.batch_header.parse(line);
                     file.batches.push(batch);
                 }
                 "6" => {
                     debug!("detail entry found");
-                    file.last_batch().new_entry().parse(line);
+                    file.last_batch().new_entry(line);
                 }
                 "7" => {
                     debug!("addendum entry found");
-                    file.last_batch().last_entry().new_addenda().parse(line);
+                    file.last_batch().last_entry().add_addenda(line);
                 }
                 "9" => {
                     debug!("file control found");
-                    file.file_control = FileControl {
-                        ..Default::default()
-                    };
-                    file.file_control.parse(line);
+                    file.file_control = Some(FileControl::parse(line));
                     break;
                 }
                 _ => debug!("unknown record found"),
@@ -63,13 +60,13 @@ impl NachaFile {
     }
 }
 
-#[derive(Debug, Default, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct FileHeader {
     pub record_type_code: String,
     pub priority_code: String,
     pub immediate_destination: String,
     pub immediate_origin: String,
-    pub file_creation_date: String,
+    pub file_creation_date: NaiveDate,
     pub file_creation_time: String,
     pub file_id_modifier: String,
     pub record_size: String,
@@ -81,24 +78,28 @@ pub struct FileHeader {
 }
 
 impl FileHeader {
-    pub fn parse(&mut self, line: String) {
-        self.record_type_code = line[0..1].trim().to_string();
-        self.priority_code = line[1..3].trim().to_string();
-        self.immediate_destination = line[3..13].trim().to_string();
-        self.immediate_origin = line[13..23].trim().to_string();
-        self.file_creation_date = line[23..29].trim().to_string();
-        self.file_creation_time = line[29..33].trim().to_string();
-        self.file_id_modifier = line[33..34].trim().to_string();
-        self.record_size = line[34..37].trim().to_string();
-        self.blocking_factor = line[37..39].trim().to_string();
-        self.format_code = line[39..40].trim().to_string();
-        self.immediate_destination_name = line[40..63].trim().to_string();
-        self.immediate_origin_name = line[63..86].trim().to_string();
-        self.reference_code = line[86..94].trim().to_string();
+    pub fn parse(line: String) -> FileHeader {
+        let date = line[23..29].trim();
+        let fh = FileHeader {
+            record_type_code: line[0..1].trim().to_string(),
+            priority_code: line[1..3].trim().to_string(),
+            immediate_destination: line[3..13].trim().to_string(),
+            immediate_origin: line[13..23].trim().to_string(),
+            file_creation_date: NaiveDate::parse_from_str(date, "%y%m%d").unwrap(),
+            file_creation_time: line[29..33].trim().to_string(),
+            file_id_modifier: line[33..34].trim().to_string(),
+            record_size: line[34..37].trim().to_string(),
+            blocking_factor: line[37..39].trim().to_string(),
+            format_code: line[39..40].trim().to_string(),
+            immediate_destination_name: line[40..63].trim().to_string(),
+            immediate_origin_name: line[63..86].trim().to_string(),
+            reference_code: line[86..94].trim().to_string(),
+        };
+        return fh;
     }
 }
 
-#[derive(Debug, Default, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Batch {
     pub batch_header: BatchHeader,
     pub detail_entries: Vec<DetailEntry>,
@@ -106,10 +107,9 @@ pub struct Batch {
 }
 
 impl Batch {
-    pub fn new_entry(&mut self) -> &mut DetailEntry {
-        let detail: DetailEntry = Default::default();
+    pub fn new_entry(&mut self, line: String) {
+        let detail = DetailEntry::parse(line);
         self.detail_entries.push(detail);
-        return self.detail_entries.last_mut().unwrap();
     }
 
     pub fn last_entry(&mut self) -> &mut DetailEntry {
@@ -117,7 +117,7 @@ impl Batch {
     }
 }
 
-#[derive(Debug, Default, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct BatchHeader {
     pub record_type_code: String,
     pub service_class_code: String,
@@ -135,24 +135,27 @@ pub struct BatchHeader {
 }
 
 impl BatchHeader {
-    pub fn parse(&mut self, line: String) {
-        self.record_type_code = line[0..1].trim().to_string();
-        self.service_class_code = line[1..4].trim().to_string();
-        self.company_name = line[4..20].trim().to_string();
-        self.company_discretionary_data = line[20..40].trim().to_string();
-        self.company_id = line[40..50].trim().to_string();
-        self.standard_entry_class_code = line[50..53].trim().to_string();
-        self.company_entry_description = line[53..63].trim().to_string();
-        self.company_descriptive_date = line[63..69].trim().to_string();
-        self.effective_entry_date = line[69..75].trim().to_string();
-        self.settlement_date = line[75..78].trim().to_string();
-        self.originator_status_code = line[78..79].trim().to_string();
-        self.originating_dfi_id = line[79..87].trim().to_string();
-        self.batch_number = line[87..94].trim().to_string();
+    pub fn parse(line: String) -> BatchHeader {
+        let bh = BatchHeader {
+            record_type_code: line[0..1].trim().to_string(),
+            service_class_code: line[1..4].trim().to_string(),
+            company_name: line[4..20].trim().to_string(),
+            company_discretionary_data: line[20..40].trim().to_string(),
+            company_id: line[40..50].trim().to_string(),
+            standard_entry_class_code: line[50..53].trim().to_string(),
+            company_entry_description: line[53..63].trim().to_string(),
+            company_descriptive_date: line[63..69].trim().to_string(),
+            effective_entry_date: line[69..75].trim().to_string(),
+            settlement_date: line[75..78].trim().to_string(),
+            originator_status_code: line[78..79].trim().to_string(),
+            originating_dfi_id: line[79..87].trim().to_string(),
+            batch_number: line[87..94].trim().to_string(),
+        };
+        return bh;
     }
 }
 
-#[derive(Debug, Default, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct DetailEntry {
     pub record_type_code: String,
     pub transaction_code: String,
@@ -169,28 +172,31 @@ pub struct DetailEntry {
 }
 
 impl DetailEntry {
-    pub fn parse(&mut self, line: String) {
-        self.record_type_code = line[0..1].trim().to_string();
-        self.transaction_code = line[1..3].trim().to_string();
-        self.receiving_dfi_id = line[3..11].trim().to_string();
-        self.check_digit = line[11..12].trim().to_string();
-        self.dfi_account_number = line[12..29].trim().to_string();
-        self.amount = line[29..39].trim().parse().unwrap();
-        self.individual_id_number = line[39..54].trim().to_string();
-        self.individual_name = line[54..76].trim().to_string();
-        self.discretionary_data = line[76..78].trim().to_string();
-        self.addenda_record_indicator = line[78..79].trim().to_string();
-        self.trace_number = line[79..94].trim().to_string();
+    pub fn parse(line: String) -> DetailEntry {
+        let entry = DetailEntry {
+            record_type_code: line[0..1].trim().to_string(),
+            transaction_code: line[1..3].trim().to_string(),
+            receiving_dfi_id: line[3..11].trim().to_string(),
+            check_digit: line[11..12].trim().to_string(),
+            dfi_account_number: line[12..29].trim().to_string(),
+            amount: line[29..39].trim().parse().unwrap(),
+            individual_id_number: line[39..54].trim().to_string(),
+            individual_name: line[54..76].trim().to_string(),
+            discretionary_data: line[76..78].trim().to_string(),
+            addenda_record_indicator: line[78..79].trim().to_string(),
+            trace_number: line[79..94].trim().to_string(),
+            addenda: Vec::new(),
+        };
+        return entry;
     }
 
-    pub fn new_addenda(&mut self) -> &mut Addendum {
-        let addendum: Addendum = Default::default();
-        self.addenda.push(addendum);
-        return self.addenda.last_mut().unwrap();
+    pub fn add_addenda(&mut self, line: String) {
+        let new_addendum = Addendum::parse(line);
+        self.addenda.push(new_addendum);
     }
 }
 
-#[derive(Debug, Default, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Addendum {
     record_type_code: String,
     addenda_type_code: String,
@@ -200,16 +206,19 @@ pub struct Addendum {
 }
 
 impl Addendum {
-    pub fn parse(&mut self, line: String) {
-        self.record_type_code = line[0..1].trim().to_string();
-        self.addenda_type_code = line[1..3].trim().to_string();
-        self.payment_related_info = line[3..83].trim().to_string();
-        self.addenda_sequence_number = line[83..87].trim().to_string();
-        self.entry_detail_sequence_number = line[87..94].trim().to_string();
+    pub fn parse(line: String) -> Addendum {
+        let a = Addendum {
+            record_type_code: line[0..1].trim().to_string(),
+            addenda_type_code: line[1..3].trim().to_string(),
+            payment_related_info: line[3..83].trim().to_string(),
+            addenda_sequence_number: line[83..87].trim().to_string(),
+            entry_detail_sequence_number: line[87..94].trim().to_string(),
+        };
+        return a;
     }
 }
 
-#[derive(Debug, Default, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct FileControl {
     pub record_type_code: String,
     pub batch_count: i32,
@@ -222,14 +231,17 @@ pub struct FileControl {
 }
 
 impl FileControl {
-    pub fn parse(&mut self, line: String) {
-        self.record_type_code = line[0..1].trim().to_string();
-        self.batch_count = line[1..7].trim().parse().unwrap();
-        self.block_count = line[7..13].trim().parse().unwrap();
-        self.entry_and_addenda_count = line[13..21].trim().parse().unwrap();
-        self.entry_hash = line[21..31].trim().to_string();
-        self.total_debit = line[31..43].trim().parse().unwrap();
-        self.total_credit = line[43..55].trim().parse().unwrap();
-        self.reserved = line[55..94].trim().to_string();
+    pub fn parse(line: String) -> FileControl {
+        let fc = FileControl {
+            record_type_code: line[0..1].trim().to_string(),
+            batch_count: line[1..7].trim().parse().unwrap(),
+            block_count: line[7..13].trim().parse().unwrap(),
+            entry_and_addenda_count: line[13..21].trim().parse().unwrap(),
+            entry_hash: line[21..31].trim().to_string(),
+            total_debit: line[31..43].trim().parse().unwrap(),
+            total_credit: line[43..55].trim().parse().unwrap(),
+            reserved: line[55..94].trim().to_string(),
+        };
+        return fc;
     }
 }
