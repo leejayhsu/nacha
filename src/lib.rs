8442d45fae11c6b9ac8c6116f51f7512dba1739e
Leejay Hsu <leejay@bond.tech>
2022-06-22 22:21:23 -0700

    trim fields

--- src/lib.rs
+++ src/lib.rs
@@ -81,19 +81,19 @@ pub struct FileHeader {
 
 impl FileHeader {
     pub fn parse(&mut self, line: String) {
-        self.record_type_code = line[0..1].to_string();
-        self.priority_code = line[1..3].to_string();
-        self.immediate_destination = line[3..13].to_string();
-        self.immediate_origin = line[13..23].to_string();
-        self.file_creation_date = line[23..29].to_string();
-        self.file_creation_time = line[29..33].to_string();
-        self.file_id_modifier = line[33..34].to_string();
-        self.record_size = line[34..37].to_string();
-        self.blocking_factor = line[37..39].to_string();
-        self.format_code = line[39..40].to_string();
-        self.immediate_destination_name = line[40..63].to_string();
-        self.immediate_origin_name = line[63..86].to_string();
-        self.reference_code = line[86..94].to_string();
+        self.record_type_code = line[0..1].trim().to_string();
+        self.priority_code = line[1..3].trim().to_string();
+        self.immediate_destination = line[3..13].trim().to_string();
+        self.immediate_origin = line[13..23].trim().to_string();
+        self.file_creation_date = line[23..29].trim().to_string();
+        self.file_creation_time = line[29..33].trim().to_string();
+        self.file_id_modifier = line[33..34].trim().to_string();
+        self.record_size = line[34..37].trim().to_string();
+        self.blocking_factor = line[37..39].trim().to_string();
+        self.format_code = line[39..40].trim().to_string();
+        self.immediate_destination_name = line[40..63].trim().to_string();
+        self.immediate_origin_name = line[63..86].trim().to_string();
+        self.reference_code = line[86..94].trim().to_string();
     }
 }
 
@@ -135,19 +135,19 @@ pub struct BatchHeader {
 
 impl BatchHeader {
     pub fn parse(&mut self, line: String) {
-        self.record_type_code = line[0..1].to_string();
-        self.service_class_code = line[1..4].to_string();
-        self.company_name = line[4..20].to_string();
-        self.company_discretionary_data = line[20..40].to_string();
-        self.company_id = line[40..50].to_string();
-        self.standard_entry_class_code = line[50..53].to_string();
-        self.company_entry_description = line[53..63].to_string();
-        self.company_descriptive_date = line[63..69].to_string();
-        self.effective_entry_date = line[69..75].to_string();
-        self.settlement_date = line[75..78].to_string();
-        self.originator_status_code = line[78..79].to_string();
-        self.originating_dfi_id = line[79..87].to_string();
-        self.batch_number = line[87..94].to_string();
+        self.record_type_code = line[0..1].trim().to_string();
+        self.service_class_code = line[1..4].trim().to_string();
+        self.company_name = line[4..20].trim().to_string();
+        self.company_discretionary_data = line[20..40].trim().to_string();
+        self.company_id = line[40..50].trim().to_string();
+        self.standard_entry_class_code = line[50..53].trim().to_string();
+        self.company_entry_description = line[53..63].trim().to_string();
+        self.company_descriptive_date = line[63..69].trim().to_string();
+        self.effective_entry_date = line[69..75].trim().to_string();
+        self.settlement_date = line[75..78].trim().to_string();
+        self.originator_status_code = line[78..79].trim().to_string();
+        self.originating_dfi_id = line[79..87].trim().to_string();
+        self.batch_number = line[87..94].trim().to_string();
     }
 }
 
@@ -169,17 +169,17 @@ pub struct DetailEntry {
 
 impl DetailEntry {
     pub fn parse(&mut self, line: String) {
-        self.record_type_code = line[0..1].to_string();
-        self.transaction_code = line[1..3].to_string();
-        self.receiving_dfi_id = line[3..11].to_string();
-        self.check_digit = line[11..12].to_string();
-        self.dfi_account_number = line[12..29].to_string();
-        self.amount = line[29..39].to_string();
-        self.individual_id_number = line[39..54].to_string();
-        self.individual_name = line[54..76].to_string();
-        self.discretionary_data = line[76..78].to_string();
-        self.addenda_record_indicator = line[78..79].to_string();
-        self.trace_number = line[79..94].to_string();
+        self.record_type_code = line[0..1].trim().to_string();
+        self.transaction_code = line[1..3].trim().to_string();
+        self.receiving_dfi_id = line[3..11].trim().to_string();
+        self.check_digit = line[11..12].trim().to_string();
+        self.dfi_account_number = line[12..29].trim().to_string();
+        self.amount = line[29..39].trim().to_string();
+        self.individual_id_number = line[39..54].trim().to_string();
+        self.individual_name = line[54..76].trim().to_string();
+        self.discretionary_data = line[76..78].trim().to_string();
+        self.addenda_record_indicator = line[78..79].trim().to_string();
+        self.trace_number = line[79..94].trim().to_string();
     }
 
     pub fn new_addenda(&mut self) -> &mut Addendum {
@@ -200,11 +200,11 @@ pub struct Addendum {
 
 impl Addendum {
     pub fn parse(&mut self, line: String) {
-        self.record_type_code = line[0..1].to_string();
-        self.addenda_type_code = line[1..3].to_string();
-        self.payment_related_info = line[3..83].to_string();
-        self.addenda_sequence_number = line[83..87].to_string();
-        self.entry_detail_sequence_number = line[87..94].to_string();
+        self.record_type_code = line[0..1].trim().to_string();
+        self.addenda_type_code = line[1..3].trim().to_string();
+        self.payment_related_info = line[3..83].trim().to_string();
+        self.addenda_sequence_number = line[83..87].trim().to_string();
+        self.entry_detail_sequence_number = line[87..94].trim().to_string();
     }
 }
 
@@ -222,13 +222,13 @@ pub struct FileControl {
 
 impl FileControl {
     pub fn parse(&mut self, line: String) {
-        self.record_type_code = line[0..1].to_string();
-        self.batch_count = line[1..7].to_string();
-        self.block_count = line[7..13].to_string();
-        self.entry_and_addenda_count = line[13..21].to_string();
-        self.entry_hash = line[21..31].to_string();
-        self.total_debit = line[31..43].to_string();
-        self.total_credit = line[43..55].to_string();
-        self.reserved = line[55..94].to_string();
+        self.record_type_code = line[0..1].trim().to_string();
+        self.batch_count = line[1..7].trim().to_string();
+        self.block_count = line[7..13].trim().to_string();
+        self.entry_and_addenda_count = line[13..21].trim().to_string();
+        self.entry_hash = line[21..31].trim().to_string();
+        self.total_debit = line[31..43].trim().to_string();
+        self.total_credit = line[43..55].trim().to_string();
+        self.reserved = line[55..94].trim().to_string();
     }
 }
